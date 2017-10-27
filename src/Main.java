5b13cc1a07fb70bdb7cc3687bb4e5be8191ee398
Ricard Zarco Badia <kyriegorrin@gmail.com>
2017-10-27 09:24:36 +0200

    Intent de fix de pujada nanithefuck

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String [] args){
+        String comanda;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserta comanda (Escriu help si necessites ajuda):");
+        comanda = scanner.nextLine();
+
+        while(!comanda.equals("exit")){
+            switch(comanda){
+                case "help":
+                    System.out.println("Aqui tens les diferents comandes:");
+                    System.out.println("help -> Mostra ajuda");
+                    System.out.println("inicia -> Inicia una partida");
+                    //TODO
+                    break;
+            }
+
+        }
+    }
 }

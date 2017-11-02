6edad4d1a6ab88b0f7ecd6e3bb65f0d5dafb259c
Ricard Zarco Badia <kyriegorrin@gmail.com>
2017-11-02 17:07:06 +0100

    Afegit Driver de Jugador

---
+++ src/Drivers/DriverJugador.java
@@ -0,0 +1,11 @@
+package Drivers;
+
+import Domini.Jugador;
+import java.util.Scanner;
+
+public class DriverJugador {
+    private String comanda;
+    private Scanner reader = new Scanner(System.in);
+
+    
+}

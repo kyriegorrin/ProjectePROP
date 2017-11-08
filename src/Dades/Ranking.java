7a378e758c8e13fd9265a721cf654d35e031230d
Ricard Zarco Badia <kyriegorrin@gmail.com>
2017-11-09 00:33:28 +0100

    Ranking i driver associat acabat. Ara per ara, si algu modifica l'arxiu de ranking pel seu compte externament, explota. NO FER-HO.

--- src/Dades/Ranking.java
+++ src/Dades/Ranking.java
@@ -8,11 +8,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
+
 public class Ranking {
+    private static final int MAXJUGADORS = 10;
 
     private ArrayList<String> nomJugadors;
     private ArrayList<Integer> puntuacioJugadors;
-    boolean init; //Indica si s'ha inicialitzat
+    private boolean init; //Indica si s'ha inicialitzat
+
+    private File file;
 
     public Ranking(){
         nomJugadors = new ArrayList<String>();
@@ -20,61 +24,121 @@ public class Ranking {
         init = false;
     }
 
-    private void test(){
-        try{
-            String verify, putData;
-            File file = new File("file.txt");
-            file.createNewFile();
-            FileWriter fw = new FileWriter(file);
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.write("Some text here for a reason");
-            bw.flush();
-            bw.close();
-            FileReader fr = new FileReader(file);
-            BufferedReader br = new BufferedReader(fr);
-
-            while( (verify=br.readLine()) != null ){ //***editted
-                //**deleted**verify = br.readLine();**
-                if(verify != null){ //***edited
-                    putData = verify.replaceAll("here", "there");
-                    bw.write(putData);
-                }
-            }
-            br.close();
-
-
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-    }
-
     //Primera funcio a cridar quan es crea el ranking, llegeix l'arxiu que conté el ranking si existeix.
     //Si l'arxiu no existeix, el crea.
     public void inicialitza(){
-        String linia;
-        File file = new File("ranking.txt");
-        String [] componentsLinia;
+        if(!init){
+            String linia;
+            file = new File("ranking.txt");
+            String [] componentsLinia;
 
-        if(file.exists()){
-            //Simplement llegim linies fins que no en trobem mes
-            try{
-                FileReader fr = new FileReader(file);
-                BufferedReader br = new BufferedReader(fr);
+            if(file.exists()){
+                //Simplement llegim linies fins que no en trobem mes
+                try{
+                    FileReader fr = new FileReader(file);
+                    BufferedReader br = new BufferedReader(fr);
 
-                while((linia = br.readLine()) != null){
-                    componentsLinia = linia.split(" ");
-                //TODO:omplir les variables de classe amb lo llegit a cada linia
+                    //Llegim linies fins que s'acaba l'arxiu
+                    while((linia = br.readLine()) != null){
+                        //Tractem la linia i afegim el contingut a les variables de classe
+                        componentsLinia = linia.split(" ");
+                        nomJugadors.add(componentsLinia[0]);
+                        puntuacioJugadors.add(Integer.parseInt(componentsLinia[1]));
+                    }
+                }catch(IOException e){
+                    e.printStackTrace();
                 }
-
-            }catch(IOException e){
-                e.printStackTrace();
+            }else{
+                //Creem arxiu
+                try{
+                    file.createNewFile();
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
             }
+            //Indiquem que tot ha estat inicialitzat
+            init = true;
         }else{
-            //Creem arxiu
+            System.out.println("Ja has inicialitzat el ranking!");
         }
     }
 
+    //Inserta un jugador de manera ordenada al ranking si ha estat incialitzat abans
     public void insertaJugador(String jugador){
+        String[] componentsJugador = jugador.split(" ");
+        int puntuacio = Integer.parseInt(componentsJugador[1]);
 
+        //Si els atributs de classe no estan inicialitzats, error
+        if (!init){
+            System.out.println("Inicialitza el ranking abans de fer res!");
+        }else{
+            //Mirem si el ranking està buit, en cas positiu afegim directament
+            if (nomJugadors.isEmpty()){
+                nomJugadors.add(componentsJugador[0]);
+                puntuacioJugadors.add(puntuacio);
+            }else{
+                //Si no, busquem quin és el seu lloc i l'insertem
+                boolean trobat = false;
+                int i;
+
+                for(i = 0; i < puntuacioJugadors.size() && !trobat; ++i){
+                    if(puntuacioJugadors.get(i) < puntuacio) trobat = true;
+                }
+                //Insertem a la posicio que toca
+                if(trobat){
+                    nomJugadors.add(i-1, componentsJugador[0]);
+                    puntuacioJugadors.add(i-1, puntuacio);
+                }else{
+                    nomJugadors.add(componentsJugador[0]);
+                    puntuacioJugadors.add(puntuacio);
+                }
+            }
+            //Un cop modificat, sobreescribim els canvis a l'arxiu
+            flushRanking();
+        }
+    }
+
+    //Metode que borra el ranking complet a nivell d'arxiu i descarta els atributs de classe.
+    //Es necessari tornar a inicialitzar un cop s'ha cridat si es vol continuar utilitzant el ranking.
+    public void clear(){
+        if(file.exists()){
+            file.delete();
+        }
+        init = false;
+        nomJugadors = new ArrayList<String>();
+        puntuacioJugadors = new ArrayList<Integer>();
+    }
+
+    //Metode que retorna un string contenint el ranking, llest per printar
+    public String toString(){
+        String stringRanking = "";
+        if(init){
+            for(int i = 0; i < nomJugadors.size() && i < MAXJUGADORS; ++i){
+                stringRanking += (i+1) + " " + nomJugadors.get(i) + " " + puntuacioJugadors.get(i) +"\n";
+            }
+        }else{
+            System.out.println("Ranking no inicialitzat!");
+        }
+        return stringRanking;
+    }
+
+    //Metode intern per a actualitzar (sobrescriure) l'arxiu que conte el ranking
+    //Sempre escriu un maxim de MAXJUGADORS jugadors
+    private void flushRanking(){
+        try{
+            file.delete();
+            file.createNewFile();
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            for(int i = 0; i < nomJugadors.size() && i < MAXJUGADORS; ++i){
+                bw.write(nomJugadors.get(i) + " " + String.valueOf(puntuacioJugadors.get(i)));
+                bw.newLine();
+            }
+            bw.flush();
+            bw.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
--- src/Drivers/DriverRanking.java
+++ src/Drivers/DriverRanking.java
@@ -3,14 +3,61 @@ package Drivers;
 import Domini.Jugador;
 import Dades.Ranking;
 
+import java.util.Scanner;
+
+//TODO: afegir comandes pendents
 public class DriverRanking {
-    public static void main (String[] args){
+    public static void main (String[] args) {
+
+        String comanda;
+        Scanner reader = new Scanner(System.in);
+        Jugador[] jugadors = new Jugador[5];
+        Ranking ranking = new Ranking();
+
         //Inicialitza jugadors stud
-        Jugador [] jugadors = new Jugador[5];
-        for(int i = 0; i < 5; ++i){
-            jugadors[i] = new Jugador("Jugador" + i, i*100);
+        for (int i = 0; i < 5; ++i) {
+            jugadors[i] = new Jugador("Jugador" + i, i * 100);
         }
 
+        System.out.println("Inserta comanda (escriu help si necessites ajuda):");
+        comanda = reader.nextLine();
 
+        while (!comanda.equals("exit")) {
+            switch (comanda) {
+                case "help":
+                    System.out.println("Llista de comandes:");
+                    System.out.println("inicialitza -> Inicialitza la classe i llegeix les dades guardades");
+                    System.out.println("insertaJugador -> Afegeix un jugador de manera ordenada");
+                    System.out.println("clear -> Esborra tot el ranking (arxiu inclos). Necessita inicialitzar despres de cridar aquest metode");
+                    System.out.println("toString -> Crea un string que representa el ranking (i en aquest cas el printa)\n");
+                    System.out.println("---IMPORTANT: Inicialitza el ranking abans de operar amb ell. " +
+                                        "La constructora no te parametres, aixi que es crida per defecte---\n");
+                    System.out.println("Disposes de 5 Jugadors stud, amb identificadors del 0 fins al 4, amb puntuacions equivalents a id*100\n");
+                    break;
+
+                case "inicialitza":
+                    ranking.inicialitza();
+                    break;
+
+                case "insertaJugador":
+                    System.out.println("Escriu el id del jugador a afegir (0..4)");
+                    ranking.insertaJugador(jugadors[reader.nextInt()].toString());
+                    break;
+
+                case "clear":
+                    ranking.clear();
+                    break;
+
+                case "toString":
+                    System.out.println(ranking.toString());
+                    break;
+
+                default:
+                    System.out.println("Comanda incorrecta");
+                    break;
+            }
+            System.out.println("Inserta comanda (escriu help si necessites ajuda):");
+            comanda = reader.nextLine();
+        }
     }
 }

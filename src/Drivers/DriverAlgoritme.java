98ba06f17884b29444a06927fafdf85202e22715
LordKikkoman <33098680+LordKikkoman@users.noreply.github.com>
2017-11-11 18:46:51 +0100

    Add files via upload

---
+++ src/Drivers/DriverAlgoritme.java
@@ -0,0 +1,32 @@
+package Drivers;
+
+import Domini.Algoritme;
+import Domini.Combinacio;
+import Domini.Tauler;
+
+import java.util.Scanner;
+
+public class DriverAlgoritme {
+
+	public static void main(String [] args){
+		Tauler t = new Tauler(8, 4, 8);
+		Combinacio c = new Combinacio(t.getLine_size());
+		c.llegir_comb();
+		t.setInitial_line(c);
+		Algoritme a = new Algoritme(t);
+		int cont = 0;
+		
+		while (t.getUltima() >= 0) {
+			System.out.println("iteracio:"+cont);
+			c = a.algoritmeGenetic(t);
+			c.escriu_combinacio();
+			System.out.println("TAULER");
+			t.set_ultima_linia(c);
+			t.escriu_tauler();
+			System.out.println("----------------");
+			t.escriu_solucio();
+			++cont;
+			System.out.println();
+		}
+	}
+}

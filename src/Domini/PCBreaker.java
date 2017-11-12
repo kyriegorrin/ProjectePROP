fe4f795674b658ac051a5c39209b0228c9e4a299
marcazu <32565117+marcazu@users.noreply.github.com>
2017-11-12 10:47:29 +0100

    Update PCBreaker.java

--- src/Domini/PCBreaker.java
+++ src/Domini/PCBreaker.java
@@ -1,16 +1,32 @@
 package Domini;
 
+/**
+ * 
+ * CLASE pcbREAKER
+ *
+ */
 public class PCBreaker extends Jugador{
-    private Algoritme algoritme;
+    private Algoritme algoritme; 
 
+    /**
+     * Constructora que crea un PCBreaker buit.
+     */
     public PCBreaker() {
     }
 
+    /**
+     * Constructora que crea un PCBreaker inicialitzat.
+     * @param nom Es un string inicialitzat.
+     * @param puntuacio Es un enter >= 0.
+     */
     public PCBreaker(String nom, int puntuacio) {
         super(nom, puntuacio);
         algoritme = new Algoritme();
     }
 
+    /**
+     * Es sobrescriu la funcio del pare de fer jugada.
+     */
     @Override
     public Combinacio fesJugada(Tauler tauler){
         Combinacio combinacio = algoritme.algoritmeGenetic(tauler);

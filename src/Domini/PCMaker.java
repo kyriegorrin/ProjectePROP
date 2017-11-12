ced0baae903ff5f5f908926d38e2ef46f78dbfaa
marcazu <32565117+marcazu@users.noreply.github.com>
2017-11-12 10:44:34 +0100

    Update PCMaker.java

--- src/Domini/PCMaker.java
+++ src/Domini/PCMaker.java
@@ -1,17 +1,34 @@
 package Domini;
 
+/**
+ * CLASE PC MAKER
+ */
 import java.util.Random;
 
 public class PCMaker extends Jugador {
+	
+	/**
+	 * Constructora que crea un PCMaker vuit.
+	 */
 
     public PCMaker() {
     }
 
+    /**
+     * Constructora que crea un PCMaker pasant-li els parametres al pare.
+     * @param nom String Inicializat.
+     * @param puntuacio Enter inicialitzat >= 0.
+     */
     public PCMaker(String nom, int puntuacio) {
         super(nom, puntuacio);
     }
 
-    //Retorna la combinació inicial del tauler (la solucio). En aquest cas es random dintre del rang de colors.
+    /**
+     * Retorna la combinació inicial del tauler (la solucio). En aquest cas es random dintre del rang de colors.
+     * @param tamany Enter > 0.
+     * @param colors Enter > 0.
+     * @returnEs retorna la combinacio inicial del tauler.
+     */
     public Combinacio triaCombinacio(int tamany, int colors){
         Combinacio combinacio = new Combinacio(tamany);
 

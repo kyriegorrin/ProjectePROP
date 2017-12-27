108263c635642b28c7cd80c2d38df479e2ccc883
Ricard Zarco Badia <kyriegorrin@gmail.com>
2017-12-27 13:53:18 +0100

    Formulari de Helper acabat, falta implementar-ne la lògica

--- src/Interface/HelpForm.java
+++ src/Interface/HelpForm.java
@@ -4,5 +4,14 @@ import javax.swing.*;
 
 public class HelpForm {
     private JPanel panel1;
-    private JTextArea benvingutsAMasterMindElTextArea;
+    private JTextPane benvingutsAMasterMindElTextPane;
+    private JButton tornarButton;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Ajuda");
+        frame.setContentPane(new HelpForm().panel1);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
